aa783432b1526e912ffaaec87eff18a0ab2c6302
iris <iris@datatree.co.kr>
2021-06-09 15:22:38 +0900

    문자열 압축

---
+++ 2020-kakao-blind/src/Application.java
@@ -0,0 +1,9 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		Q1 q1 = new Q1();
+		System.out.println(q1.solution("a"));
+	}
+
+}
---
+++ 2020-kakao-blind/src/Q1.java
@@ -0,0 +1,57 @@
+public class Q1 {
+	public int solution(String s) { // 1 이상 1,000 이하
+
+		int min = 1001;
+		
+		int ii = (s.length()/2 < 1) ? 1 : s.length()/2; // 만약 input 이 1글자일 경우
+		for (int i = 1; i <= ii; i++) { // 압축 개수
+
+			boolean last = false; 
+			int cnt = 1; //중복 횟수
+			StringBuilder candi = new StringBuilder();
+			
+			for (int j = 0; j < s.length(); j = j + i) { //j = 0
+				
+				String str = ""; //현재값
+				String key = ""; //다음값
+				if (j + i >= s.length()) { // 나머지 문자열
+					str = s.substring(j); 
+					last = true;
+				} else {
+					str = s.substring(j, j + i); 
+				}
+				
+				if (!last) {
+					if(j+i+i >= s.length())
+						key = s.substring(j+i); 
+					else
+						key = s.substring(j + i, j + i + i); 
+				}
+					
+				if (str.equals(key)) { // 압축이 된다면
+					if (last && cnt > 1) {
+						candi.append(String.valueOf(cnt)); //중복개수가 10개 이상. 즉 2자리일 수도 있음 ***
+						candi.append(String.valueOf(key));
+					}else if (last && cnt == 1) {
+						candi.append(String.valueOf(key)); 
+					}
+					cnt++; 
+				} else { 
+					if (cnt > 1) {
+						candi.append(String.valueOf(cnt)); 
+						candi.append(String.valueOf(str)); 
+					} else {
+						candi.append(String.valueOf(str));
+					}
+
+					cnt = 1;
+				}
+			}//for j
+
+			min = Math.min(candi.length(), min);
+		}
+
+		int answer = min;
+		return answer;
+	}
+}
